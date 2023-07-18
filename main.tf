c48611914ee677db3ccc4b11150242944610b2cb
Suraj Mandal <surajmandalcell@gmail.com>
2023-07-18 17:39:37 -0400

    use count since the x86 are identical images

--- main.tf
+++ main.tf
@@ -47,7 +47,9 @@ resource "oci_core_volume" "block-volume-1" {
 #      }
 # }
 
-resource "oci_core_instance" "instance-2" {
+resource "oci_core_instance" "instance" {
+  count = 2
+
   availability_domain = local.availability_domain
   compartment_id      = local.compartment_id
   display_name        = local.instance_display_name
@@ -60,37 +62,17 @@ resource "oci_core_instance" "instance-2" {
 
   create_vnic_details {
     subnet_id        = oci_core_subnet.subnet.id
-    display_name     = "x86-vnic-1"
+    display_name     = "x86-vnic-${count.index + 1}"
     assign_public_ip = true
   }
 
   source_details {
-    source_type = "image"
-    source_id   = local.source_id_x86
+    source_type            = "image"
+    source_id              = local.source_id_x86
     boot_volume_size_in_gbs = 50
   }
-}
 
-resource "oci_core_instance" "instance-3" {
-  availability_domain = local.availability_domain
-  compartment_id      = local.compartment_id
-  display_name        = local.instance_display_name
-  shape               = "VM.Standard.E2.1.Micro"
-
-  shape_config {
-    memory_in_gbs = 1
-    ocpus         = 1
-  }
-
-  create_vnic_details {
-    subnet_id        = oci_core_subnet.subnet.id
-    display_name     = "x86-vnic-2"
-    assign_public_ip = true
-  }
-
-  source_details {
-    source_type = "image"
-    source_id   = local.source_id_x86
-    boot_volume_size_in_gbs = 50
+  lifecycle {
+    prevent_destroy = true
   }
 }
