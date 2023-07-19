f0da581a149327fe69f61f6c0cd02d5a8515bbd7
Suraj Mandal <surajmandalcell@gmail.com>
2023-07-19 12:51:32 -0400

    Update

--- main.tf
+++ main.tf
@@ -12,34 +12,50 @@ resource "oci_core_volume" "block-volume-1" {
   size_in_gbs         = 50
 }
 
-# resource "oci_core_instance" "instance-1" {
-#   availability_domain = local.availability_domain
-#   compartment_id      = local.compartment_id
-#   display_name        = "${local.instance_display_name}-1"
-#   shape               = "VM.Standard.A1.Flex"
+resource "oci_core_instance" "instance-1" {
+  availability_domain = local.availability_domain
+  compartment_id      = local.compartment_id
+  display_name        = "${local.instance_display_name}-1"
+  shape               = "VM.Standard.A1.Flex"
 
-#   shape_config {
-#     memory_in_gbs = 24
-#     ocpus         = 4
-#   }
+  shape_config {
+    memory_in_gbs = 24
+    ocpus         = 4
+  }
 
-#   create_vnic_details {129.153.49.40
-#     subnet_id        = oci_core_subnet.subnet.id
-#     display_name     = "arm-vnic"
-#     assign_public_ip = true
-#     hostname_label   = "arm-free-tier"
-#   }
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.subnet.id
+    display_name     = "arm-vnic"
+    assign_public_ip = true
+  }
 
-#   source_details {
-#     source_type = "image"
-#     source_id   = local.source_id_arm
-#     boot_volume_size_in_gbs = 50
-#   }
-# 
-#    lifecycle {
-#        prevent_destroy = true
-#      }
-# }
+  source_details {
+    source_type             = "image"
+    source_id               = local.source_id_arm
+    boot_volume_size_in_gbs = 50
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  agent_config {
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Bastion"
+    }
+  }
+}
 
 resource "oci_core_instance" "instance-2" {
   availability_domain = local.availability_domain
@@ -131,5 +147,22 @@ resource "oci_core_instance" "instance-3" {
   metadata = {
     ssh_authorized_keys = file(local.public_key_path)
     user_data           = base64encode(local.script_1)
+  }
+
+  agent_config {
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Bastion"
+    }
   }
 }
